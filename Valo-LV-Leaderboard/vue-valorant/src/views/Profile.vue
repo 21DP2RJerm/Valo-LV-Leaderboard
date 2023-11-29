ae8c74a48bc00e20f98eec0c0e18d41bd227abd1
21DP2RJerm <12DRJermacans@rvt.lv>
2023-11-29 15:22:03 +0200

    new profile changes

--- Valo-LV-Leaderboard/vue-valorant/src/views/Profile.vue
+++ Valo-LV-Leaderboard/vue-valorant/src/views/Profile.vue
@@ -16,8 +16,18 @@ export default {
 
     <img src="../assets//pfp.png " alt="pfp" id="pfp">
     <div id="userdata">
-      <div id="name" class="font-head">Jertix</div>
+      <a href="https://tracker.gg/valorant/profile/riot/Jertix%23ists/overview" target="_blank" id="name" class="font-huge">Jertix </a>
       <img src="../assets/immo2.png" alt="rank" id="rank">
+    </div>
+    <div id="userstats">
+      <div id="stats" class="font-huge">Augstākā vieta LV leaderboard</div>
+      <div id="stats" class="font-huge">Augstākā vieta pasaules leaderboard</div>
+      <div id="stats" class="font-huge">Augstākais ranks</div>
+    </div>
+    <div id="statdata">
+      <div id="dati" class="font-huge">12</div>
+      <div id="dati" class="font-huge">14098</div>
+      <div id="dati" class="font-huge"><img src="../assets/immo2.png" alt="rank" id="rank"></div>
     </div>
     
 
@@ -52,6 +62,7 @@ export default {
   #name{
     color: aliceblue;
     margin-right: 15px;
+    text-decoration: none;
   }
   #rank{
     top: -2px;
@@ -62,5 +73,26 @@ export default {
     height: 50px;
     left: 200px;
     top: 130px;
+    align-items: center;
+  }
+
+  #userstats{
+    position: absolute;
+    display: flex;
+  }
+  #stats{
+    max-width: 46vh;
+    color: aliceblue;
+    padding: 0.3vh;
+    text-align: center;
+  }
+
+  #statdata{
+    position: absolute;
+    display: flex;
+    justify-content: space-between;
+    height: 50px;
+    top: 450px;
+    width:max-content;
   }
 </style>
